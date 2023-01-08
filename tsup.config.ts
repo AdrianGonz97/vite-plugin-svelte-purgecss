613244791f1fd9a3327965fdd9b845df61a16ecf
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-01-07 19:24:30 -0500

    removed bundling

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
-	splitting: false,
+	splitting: true,
 	sourcemap: true,
 	clean: true,
 	format: ["esm", "cjs"],
 	dts: true,
 	outDir: "dist",
-	tsconfig: "tsconfig.json",
+	skipNodeModulesBundle: true,
+	platform: "node",
 });
