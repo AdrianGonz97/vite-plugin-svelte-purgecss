a262203b31c59b9480655de5bef6f47f6a5d9b5f
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-02-10 21:27:48 -0500

    example with default import

--- tests/sveltekit-skeleton/vite.config.ts
+++ tests/sveltekit-skeleton/vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { purgeCss } from '../../dist/index.mjs';
+import purgeCss from '../../dist/index.mjs';
 
 export default defineConfig({
 	plugins: [sveltekit(), purgeCss()],
