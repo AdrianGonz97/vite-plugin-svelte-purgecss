36fe78abe9f3500d778e552c1e8223575bacb2c5
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-07-17 18:02:30 -0400

    don't need those

--- tests/sveltekit-skeleton/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import purgeCss from '../../dist/index.mjs';
-
-export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],
-});
